ae02823798bc0e9dc836ac7b957980118e5b949f
Andrew David <56502440+andydavidlee@users.noreply.github.com>
2020-08-30 17:29:24 +1000

    new

---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>hello world</h1>
+        <ul v-for="message in messages":key="array">
+            <li>{{message}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    // name: messages,
+    data: function() {
+        return {
+            messages: ["hello", "hi"]
+    };
+
+}
+}
+</script>
